a3d57c5182af975f83117fe34520beef473e3ed5
sarva <sarva@ACE5>
2024-12-09 22:53:17 +0500

    Add project files.

---
+++ namespaces.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+namespace first {
+	
+	int x = 1;
+	
+}
+
+namespace second {
+		
+	int x = 2;
+	
+}
+
+int main () {
+	
+	using namespace second;
+	
+	std::cout << x << '\n';
+	
+	return 0;
+	
+}
